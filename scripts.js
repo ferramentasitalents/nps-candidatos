0fa674c5a86d3c53f1a4a3a9f4079e3b4a9354bb
ferramentasitalents <133061937+ferramentasitalents@users.noreply.github.com>
2023-05-16 11:25:16 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
+function calculateNPS() {
+  const promotores = parseFloat(document.getElementById("promotores").value);
+  const denegridores = parseFloat(document.getElementById("denegridores").value);
+  const nps = promotores - denegridores;
+  document.getElementById("result").innerHTML = `NPS de Candidatos: ${nps.toFixed(2)}%`;
+}
 
